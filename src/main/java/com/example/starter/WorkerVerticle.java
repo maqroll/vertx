8a5d68fdd8638917cb861d059cb013b787e8b210
maqroll <loteroc@gmail.com>
2021-01-27 16:06:18 +0100

    .

---
+++ src/main/java/com/example/starter/WorkerVerticle.java
@@ -0,0 +1,30 @@
+package com.example.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorkerVerticle extends AbstractVerticle {
+  private final Logger logger = LoggerFactory.getLogger(WorkerVerticle.class);
+
+  @Override
+  public void start() {
+    vertx.setPeriodic(10_000, id -> {try {
+      logger.info("Zzz...");
+      Thread.sleep(8000);
+      logger.info("Up!");
+    } catch (InterruptedException e) {
+      logger.error("Woops", e);
+    }
+    });
+  }
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    DeploymentOptions opts = new DeploymentOptions().setInstances(2).setWorker(true);
+    vertx.deployVerticle("com.example.starter.WorkerVerticle", opts);
+  }
+}
+
