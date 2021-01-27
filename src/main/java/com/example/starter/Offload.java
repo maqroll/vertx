244e4fb0c4f26558ec26959193df49a7b9045010
maqroll <loteroc@gmail.com>
2021-01-27 16:39:07 +0100

    .

---
+++ src/main/java/com/example/starter/Offload.java
@@ -0,0 +1,44 @@
+package com.example.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Offload extends AbstractVerticle {
+  private final Logger logger = LoggerFactory.getLogger(Offload.class);
+
+  public void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle("com.example.starter.Offload");
+  }
+
+  @Override
+  public void start() {
+    vertx.setPeriodic(5000, id -> {
+      logger.info("Tick");
+      vertx.executeBlocking(this::blockingCode, this::resultHandler);
+    });
+  }
+
+  private void blockingCode(Promise<String> promise) {
+    logger.info("Blocking code running");
+    try {
+      Thread.sleep(4000);
+      logger.info("Done!");
+      promise.complete("Ok!");
+    } catch (InterruptedException e) {
+      promise.fail(e);
+    }
+  }
+
+  private void resultHandler(AsyncResult<String> ar) {
+    if (ar.succeeded()) {
+      logger.info("Blocking code result: {}", ar.result());
+    }else{
+      logger.error("Woops", ar.cause());
+    }
+  }
+}
