b110a6f3b818c3b63947e1d54cc1de079eb27f44
maqroll <loteroc@gmail.com>
2021-02-09 19:14:48 +0100

    .

--- src/main/java/com/example/tb/PublishToTBInStreamingFromKafka.java
+++ src/main/java/com/example/tb/PublishToTBInStreamingFromKafka.java
@@ -2,7 +2,6 @@ package com.example.tb;
 
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
-import io.reactivex.Maybe;
 import io.reactivex.Single;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.ext.web.client.WebClientOptions;
@@ -21,7 +20,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 public class PublishToTBInStreamingFromKafka extends AbstractVerticle {
@@ -60,46 +58,57 @@ public class PublishToTBInStreamingFromKafka extends AbstractVerticle {
     consumer
       .subscribe("data")
       .toFlowable()
+      // make sure that flow processing (including http) is serialized
+      // blocking in subscription in event-loop prevents http request from making progress
+      // so we observe on worker (with ordered to force serialization)
+      .observeOn(RxHelper.blockingScheduler(vertx,true))
       .buffer(3, TimeUnit.SECONDS, 1000) // every 3 seconds or 1000 messages (what happens first)
       .filter(e -> !e.isEmpty())
-      .flatMap(this::streamBatchToTB)
+      .doOnNext(e -> logger.error("next"))
+      .map(this::streamBatchToTB)
       .doOnError(err -> logger.error("Woops", err))
       .retryWhen(this::retryLater)
       .subscribe(resp -> {
+        resp.toFuture().get();
+        Thread.sleep(10000); // artificial delay because of tinybird's quotas
         //consumer.commit();
       });
 
   }
 
-  private Flowable<HttpResponse<String>> streamBatchToTB(List<KafkaConsumerRecord<String, String>> kafkaConsumerRecords) {
-      // Underlying webclient either sends chunked or not depending on the size.
-      // This code is going to FAIL for small files (not chunked)
-      Single<Buffer> header = Single.just(Buffer.buffer("dia;from;to;tipo;kwh;unit_price;cost\r\n"));
-      Single<Buffer> epilogue = Single.just(Buffer.buffer(
-        "\r\n" +
-          "--" + BOUNDARY + "--\r\n"));
+  private Single<HttpResponse<String>> streamBatchToTB(List<KafkaConsumerRecord<String, String>> kafkaConsumerRecords) {
+    logger.error("Starting web request");
+    // Underlying webclient either sends chunked or not depending on the size.
+    // This code is going to FAIL for small files (not chunked)
+    Single<Buffer> header = Single.just(Buffer.buffer("dia;from;to;tipo;kwh;unit_price;cost\r\n"));
+    Single<Buffer> epilogue = Single.just(Buffer.buffer(
+      "\r\n" +
+        "--" + BOUNDARY + "--\r\n"));
 
-      Single<Buffer> prologue = Single.just(Buffer.buffer("--" + BOUNDARY + "\r\n" +
-        "Content-Disposition: form-data; name=\"csv\"; filename=\"tmp.csv\"\r\n" +
-        "Content-Type: application/octet-stream\r\n" +
-        "\r\n"));
+    Buffer prologue = Buffer.buffer("--" + BOUNDARY + "\r\n" +
+      "Content-Disposition: form-data; name=\"csv\"; filename=\"tmp.csv\"\r\n" +
+      "Content-Type: application/octet-stream\r\n" +
+      "\r\n");
 
-      Flowable<Buffer> buffer =
-        Flowable.concat(prologue.toFlowable(),
-          header.toFlowable(),
-          Flowable.fromIterable(kafkaConsumerRecords).map(r -> Buffer.buffer(r.value() + "\r\n")),
-          epilogue.toFlowable());
+    Flowable<Buffer> preamble = Flowable.just(prologue);
 
-      return webClient
-        //.postAbs("http://localhost:8080/v0/datasources?name=luz")
-        .postAbs("https://api.tinybird.co/v0/datasources?name=luz")
-        .putHeader("transfer-encoding", "chunked")
-        .putHeader("Authorization", "Bearer " + authToken)
-        .putHeader("Content-Type", "multipart/form-data; boundary=" + BOUNDARY)
-        .expect(ResponsePredicate.status(200))
-        .as(BodyCodec.string())
-        .rxSendStream(buffer)
-        .toFlowable();
+    Flowable<Buffer> buffer =
+      Flowable.concat(preamble,
+        header.toFlowable(),
+        Flowable.fromIterable(kafkaConsumerRecords).map(r -> Buffer.buffer(r.value() + "\r\n")),
+        epilogue.toFlowable());
+
+    return webClient
+      //.postAbs("http://localhost:8080/v0/datasources?name=luz")
+      .postAbs("https://api.tinybird.co/v0/datasources?name=luz&mode=append")
+      .putHeader("transfer-encoding", "chunked")
+      .putHeader("Authorization", "Bearer " + authToken)
+      .putHeader("Content-Type", "multipart/form-data; boundary=" + BOUNDARY)
+      .expect(ResponsePredicate.status(200))
+      .as(BodyCodec.string())
+      .rxSendStream(buffer)
+      ;
+      //.toFlowable();
   }
 
   private Flowable<Throwable> retryLater(Flowable<Throwable> errs) {
