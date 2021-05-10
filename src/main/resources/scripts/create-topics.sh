40d6781eed14fd97a2ca0d032ce999c35db8979f
maqroll <loteroc@gmail.com>
2021-05-10 19:53:19 +0200

    + create topics

---
+++ src/main/resources/scripts/create-topics.sh
@@ -0,0 +1,29 @@
+echo "Waiting for Kafka to come online..."
+
+cub kafka-ready -b kafka:9092 1 20
+
+kafka-topics \
+  --bootstrap-server kafka:9092 \
+  --topic input \
+  --replication-factor 1 \
+  --partitions 1 \
+  --config retention.ms=3600000 \
+  --create
+
+kafka-topics \
+  --bootstrap-server kafka:9092 \
+  --topic output \
+  --replication-factor 1 \
+  --partitions 1 \
+  --config retention.ms=3600000 \
+  --create
+
+kafka-topics \
+  --bootstrap-server kafka:9092 \
+  --topic offsets \
+  --replication-factor 1 \
+  --partitions 1 \
+  --config "cleanup.policy=compact"
+  --create
+
+sleep infinity
