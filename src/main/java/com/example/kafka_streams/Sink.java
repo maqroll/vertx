5dc965be5963d2042328c2fe4ad3a6ce99aacee9
maqroll <loteroc@gmail.com>
2021-05-11 16:42:28 +0200

    ! Sink

--- src/main/java/com/example/kafka_streams/Sink.java
+++ src/main/java/com/example/kafka_streams/Sink.java
@@ -1,6 +1,7 @@
 package com.example.kafka_streams;
 
 import io.reactivex.Completable;
+import io.reactivex.Flowable;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.shareddata.Counter;
 import io.vertx.kafka.client.common.KafkaClientOptions;
@@ -18,12 +19,14 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Sink extends AbstractVerticle {
   private static final Logger LOGGER = LoggerFactory.getLogger(Sink.class);
   private static final Random RND = new Random();
-  private final AtomicReference<KafkaProducer<byte[],byte[]>> producer = new AtomicReference<>();
+  private final AtomicReference<KafkaProducer<byte[], byte[]>> producer = new AtomicReference<>();
   private final AtomicReference<Counter> counter = new AtomicReference<>();
 
   @Override
@@ -39,61 +42,58 @@ public class Sink extends AbstractVerticle {
         this.counter.set(counter.getDelegate());
         vertx
           .eventBus()
-          .<KafkaConsumerRecord<String,String>>localConsumer("channel")
+          .<KafkaConsumerRecord<String, String>>localConsumer("channel")
           .toFlowable()
           .map(record -> record.body())
           .buffer(100, TimeUnit.MILLISECONDS, 1000)
-          .filter(kafkaConsumerRecords -> kafkaConsumerRecords.size()>0)
+          .filter(kafkaConsumerRecords -> kafkaConsumerRecords.size() > 0)
           .onBackpressureBuffer()
-          .flatMapCompletable(this::processBatchWithRetry, true, 1)
-        .subscribe();
-    }, throwable -> {
-      vertx.close();
-    });
+          .flatMapCompletable(this::processBatchRetryForever, true, 1)
+          .subscribe();
+      }, throwable -> {
+        vertx.close();
+      });
 
     return Completable.complete();
   }
 
-  private Completable processBatchWithRetry(List<KafkaConsumerRecord<String,String>> records) {
-      return vertx.rxExecuteBlocking(promise -> {
-        // Flowable.just().process.sleep.retry
-        long errors = 0;
-        counter.get().addAndGet(records.size() * -1);
+  private Completable processBatchRetryForever(List<KafkaConsumerRecord<String, String>> records) {
+    // or use a WorkerVerticle directly!!
+    return vertx.rxExecuteBlocking(promise -> {
+      AtomicBoolean inTx = new AtomicBoolean();
 
-        do {
-          try {
-            processBatch(records);
-            promise.complete();
-            break;
-          } catch (Throwable t) {
-            LOGGER.error("Error",t);
+      counter.get().addAndGet(records.size() * -1);
+
+      Flowable.just(records)
+        .doOnNext(r -> processBatch(r, inTx))
+        .doOnError(throwable -> {
+          if (inTx.get()) {
             producer.get().abortTransaction();
-            errors++;
-            LOGGER.info("Waiting for {} ms", 10L << errors);
-            final long delay = 10 << errors;
-            try {
-              Thread.sleep(10 << errors);
-            } catch (InterruptedException e) {
-              e.printStackTrace();
-            }
           }
-        } while (true);
-      }, true).ignoreElement();
+        })
+        .retryWhen(errors -> {
+          // log errors is not wrong
+          AtomicInteger counter = new AtomicInteger();
+          return errors
+            .flatMap(e -> {
+              counter.incrementAndGet();
+              return Flowable.timer(1 << counter.get(), TimeUnit.MILLISECONDS);
+            });
+        }).blockingSubscribe();
+      promise.complete();
+    },true).ignoreElement();
   }
 
-  private Completable processBatch(List<KafkaConsumerRecord<String,String>> records) {
-    if (!records.isEmpty()) {
-      long offset = records.get(records.size() - 1).offset();
-      LOGGER.info("Start processing {}", offset);
-      producer.get().beginTransaction();
-      records.stream().forEach(record -> producer.get().write(KafkaProducerRecord.create("output", record.value().getBytes())));
-      producer.get().write(KafkaProducerRecord.create("offsets", "key".getBytes(), Long.valueOf(offset + 1).toString().getBytes()));
-      injectFaultAtRandom();
-      producer.get().commitTransaction();
-      LOGGER.info("End processing {}", offset);
-    }
-
-    return Completable.complete();
+  private void processBatch(List<KafkaConsumerRecord<String, String>> records, AtomicBoolean inTx) {
+    long offset = records.get(records.size() - 1).offset();
+    LOGGER.info("Start processing {}", offset);
+    producer.get().beginTransaction();
+    inTx.set(true);
+    records.stream().forEach(record -> producer.get().write(KafkaProducerRecord.create("output", record.value().getBytes())));
+    producer.get().write(KafkaProducerRecord.create("offsets", "key".getBytes(), Long.valueOf(offset + 1).toString().getBytes()));
+    injectFaultAtRandom();
+    producer.get().commitTransaction();
+    LOGGER.info("End processing {}", offset);
   }
 
   private void injectFaultAtRandom() {
@@ -102,27 +102,12 @@ public class Sink extends AbstractVerticle {
     }
   }
 
-  private void process(KafkaConsumer<String, String> consumer) {
-    consumer.poll(Duration.ofMillis(100),event -> {
-      LOGGER.info("Poll");
-      if (event.succeeded()) {
-        KafkaConsumerRecords<String, String> records = event.result();
-        for (int i=0; i<records.size(); i++) {
-          KafkaConsumerRecord<String, String> record = records.recordAt(i);
-          LOGGER.info("{},{}", record.partition(), record.offset());
-        }
-      } else {
-        LOGGER.error("Failed while polling", event.cause());
-      }
-    });
-  }
-
   private JsonObject getConf() {
     final JsonObject props = new JsonObject();
 
     props.put("config", new JsonObject()
       .put(org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-      "kafka:9092")
+        "kafka:9092")
       //.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupTest")
       .put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
       .put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
